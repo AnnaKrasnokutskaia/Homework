dc6859c447ac8bbb690ae3ec38a6fa04b5df8683
Anna Krasnokutskaia <malepops@gmail.com>
2026-03-15 19:16:19 +0300

    решена задача 4

--- src/homework1/Homework1_4.java
+++ src/homework1/Homework1_4.java
@@ -1,4 +1,8 @@
 package homework1;
 
 public class Homework1_4 {
+    public static void main(String[] args) {
+        double n=-359.896; //кто вообще буквой n называет не натуральные числа? это извращение
+        System.out.println(Math.round(n));
+    }
 }
