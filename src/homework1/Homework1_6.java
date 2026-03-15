ec976948512e12c96a630de4a652db0c1083d5ba
Anna Krasnokutskaia <malepops@gmail.com>
2026-03-15 19:33:17 +0300

    решена задача *

--- src/homework1/Homework1_6.java
+++ src/homework1/Homework1_6.java
@@ -1,4 +1,13 @@
 package homework1;
 
 public class Homework1_6 {
+    public static void main(String[] args) {
+        int a = 31;
+        int b = 8;
+        int tmp = a;
+        a = b;
+        b = tmp;
+        System.out.println(a);
+        System.out.println(b);
+    }
 }
