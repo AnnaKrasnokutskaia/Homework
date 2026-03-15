0a0581a245ebbaeba7cd1613c686460b205c0b20
Anna Krasnokutskaia <malepops@gmail.com>
2026-03-15 18:48:39 +0300

    решена задача 1

--- src/homework1/Homework1_1.java
+++ src/homework1/Homework1_1.java
@@ -1,4 +1,11 @@
 package homework1;
 
 public class Homework1_1 {
+    public static void main(String[] args) {
+        int a;
+        int b=5;
+        int c=40;
+        a = 4*(b+c-1)/2;
+        System.out.println(a);
+    }
 }
